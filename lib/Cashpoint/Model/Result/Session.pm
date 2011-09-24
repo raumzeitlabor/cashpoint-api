614510ea3f8c0791846447875e8495fd4e972373
Simon Elsbrock <simon+rzl@iodev.org>
2011-09-24 12:13:57 +0200

    rename auth to session

---
+++ lib/Cashpoint/Model/Result/Session.pm
@@ -0,0 +1,69 @@
+package Cashpoint::Model::Result::Session;
+
+use strict;
+use warnings;
+
+use base qw/DBIx::Class::Core/;
+
+__PACKAGE__->load_components(qw/InflateColumn::DateTime/);
+__PACKAGE__->table('session');
+__PACKAGE__->add_columns(
+    sessionid => {
+        accessor  => 'session',
+        data_type => 'integer',
+        is_nullable => 0,
+        is_auto_increment => 1,
+    },
+
+    userid => {
+        accessor => 'user',
+        data_type => 'integer',
+        is_numeric => 1,
+        is_nullable => 1,
+    },
+
+    code => {
+        data_type   => 'varchar',
+        size        => 18,
+        is_nullable => 1,
+    },
+
+    cashcardid => {
+        accessor    => 'cashcard',
+        data_type   => 'id',
+        is_nullable => 1,
+    },
+
+    username => {
+        data_type   => 'varchar',
+        size        => 30,
+        is_nullable => 1,
+    },
+
+    token => {
+        data_type   => 'varchar',
+        size        => 16,
+        is_nullable => 1,
+    },
+
+    # 1 = PIN, 2 = USER/PW
+    auth_mode => {
+        data_type => 'integer',
+        is_nullable => 0,
+    },
+
+    login_date => {
+        data_type => 'datetime',
+        is_nullable => 0,
+    },
+
+    last_action => {
+        data_type => 'datetime',
+        is_nullable => 1,
+    },
+);
+
+__PACKAGE__->set_primary_key('sessionid');
+__PACKAGE__->belongs_to('cashcardid', 'Cashpoint::Model::Result::Cashcard');
+
+42;
