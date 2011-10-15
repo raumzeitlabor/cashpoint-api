f292cc0ef949eda41e889266383313acfe0dcda3
Simon Elsbrock <simon+rzl@iodev.org>
2011-10-15 02:22:24 +0200

    refuse storing purchases for composite products

--- lib/Cashpoint/API/Purchases.pm
+++ lib/Cashpoint/API/Purchases.pm
@@ -38,6 +38,9 @@ get qr{/products/([0-9]{13}|[0-9]{8})/purchases} => protected 'admin', valid_pro
 post qr{/products/([0-9]{13}|[0-9]{8})/purchases} => protected 'admin', valid_product sub {
     my $product = shift;
 
+    # check if product is composite, in which case storing purchases is prohibited
+    return status_bad_request('composite product') if ($product->composite->count);
+
     my ($supplier, $purchasedate, $expirydate, $amount, $price) =
         map { s/^\s+|\s+$//g if $_; $_ } (
             params->{supplier},
